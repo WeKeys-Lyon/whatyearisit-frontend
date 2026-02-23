931d803b43a3c808b9491b79b454727fe4a983e7
Brice WeKeys <formation@wekeys.fr>
2026-02-23 20:20:19 +0100

    Debug with year

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function displayResult() {
     fetch('https://whatyearisit-backend-psi-gules.vercel.app/year').then(response => response.json()).then(data => {
-document.querySelector('#myResult').textContent = `
+document.querySelector('#year').textContent = `
         Nous sommes dans l'année ${data.year}. Ça vous fait une belle jambe ?
 `})
 }
